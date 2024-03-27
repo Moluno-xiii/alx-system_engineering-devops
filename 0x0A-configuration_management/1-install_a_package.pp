3c9f4ccccb9b298d96e979e2af820f9399ebd42f
moluno_xiii <molunoprogress@gmail.com>
2024-03-27 03:57:44 -0700

    task 1 redo redo

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -9,3 +9,9 @@ ensure   => '2.1.0',
 provider => 'pip',
 require  => Package['python3-pip'],
 }
+
+package {'werkzeug':
+ensure   => '2.1.1',
+provider => 'pip',
+require  => Package['python3-pip'],
+}
